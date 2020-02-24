c716bcf5372c6f7249d776b3226351c3cea2667c
51260 <60729481+51260@users.noreply.github.com>
2020-02-24 12:03:36 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -9,10 +9,5 @@ def call(Map stageParams){
             case 'npm':
                 npmExecute script: script
                 break
-            case ['docker', 'kaniko']:
-                DockerUtils dockerUtils = new DockerUtils(script)
-                if (config.buildTool == 'docker' && !dockerUtils.withDockerDaemon()) {
-                    config.buildTool = 'kaniko'
-                    echo "[${STEP_NAME}] No Docker daemon available, thus switching to Kaniko build"
                 }
                 }
