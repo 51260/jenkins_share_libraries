4ea8b47ecc46e55a68488d41a6d4212067b38114
51260 <60729481+51260@users.noreply.github.com>
2020-02-25 17:30:12 +0530

    Update artifactoryUpload.groovy

--- vars/artifactoryUpload.groovy
+++ vars/artifactoryUpload.groovy
@@ -1,17 +1,15 @@
-def call() {
-rtUpload (
-                def server = Artifactory.server('Artifactory'),
-                buildName: 'DevSecOps_CICD',
-                buildNumber: Env.BUILD_ID,
-                def uploadSpec :'''{
-                    "files": [
-                        {
-                        "pattern": "${Env.PATTERN_ARTIFACTORY_FOLDER}", 
-                        "target": "${Env.TARGET_ARTIFACTORY_FOLDER}" 
-                        }
-                    ]
-                }'''
-                server.upload(uploadSpec)
-                )
-                echo 'ARTIFACT UPLOADED TO THE ARTIFACTORY'
- }
+def call {
+  
+        script { 
+            def server = Artifactory.server('Artifactory')
+
+            def uploadSpec = """{ 
+            "files": [{ 
+            "pattern": "${Env.PATTERN_ARTIFACTORY_FOLDER}", 
+            "target": "${Env.TARGET_ARTIFACTORY_FOLDER}" 
+            }] 
+            }""" 
+            server.upload(uploadSpec) 
+        } 
+        echo 'ARTIFACT UPLOADED TO THE ARTIFACTORY'
+    }
