e89f66d11dee08a75545a79b46b9c959ddfa3116
51260 <60729481+51260@users.noreply.github.com>
2020-02-25 12:30:33 +0530

    Update dependencyCheck.groovy

--- vars/dependencyCheck.groovy
+++ vars/dependencyCheck.groovy
@@ -1,5 +1,5 @@
   
 def call() {
-     dependencyCheck additionalArguments: ''' -o "./" -s "./" -f "ALL" --prettyPrint''', odcInstallation: 'OWASP-DC'
+     dependencyCheck
     dependencyCheckPublisher pattern: 'dependency-check-report.xml'
     }
