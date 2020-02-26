b54e875bcdd663fb06b24563f3f7bba77fad4ec1
51260 <60729481+51260@users.noreply.github.com>
2020-02-26 12:12:13 +0530

    Update artifactoryUpload.groovy

--- vars/artifactoryUpload.groovy
+++ vars/artifactoryUpload.groovy
@@ -2,15 +2,15 @@ def call() {
     stage("Artifact Upload") {
         script { 
             def server = Artifactory.server "Artifactory"
-            def uploadSpec = """{ 
+            def spec = """{ 
             "files": [
             { 
             "pattern": "${env.PATTERN_ARTIFACTORY_FOLDER}", 
             "target": "${env.TARGET_ARTIFACTORY_FOLDER}" 
             }
             ] 
-           }"""
-            server.upload(uploadSpec) 
+            }"""
+           server.upload(spec) 
         } 
         echo 'ARTIFACT UPLOADED TO THE ARTIFACTORY'
     }
