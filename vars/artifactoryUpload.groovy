e754c01d170144cf3be5cbf7c8413c1074963055
51260 <60729481+51260@users.noreply.github.com>
2020-02-26 09:51:32 +0530

    Update artifactoryUpload.groovy

--- vars/artifactoryUpload.groovy
+++ vars/artifactoryUpload.groovy
@@ -1,14 +1,18 @@
-def call (Map stageParams) {      
-rtUpload (
-    serverId: 'Artifactory',
-    failNoOp: true,
-    spec: '''{
-          "files": [
-            {
-              "pattern": "target/simple-spring-webapp-1.0.war",
-              "target": "DevSecOpspetclinic"
-            }
-         ]
-    }'''
-)
+import org.environment.Env;
+
+def call(Map config=[:], Closure body={}) {
+    stage("Artifact Upload") {
+        script { 
+            def server = Artifactory.server('Artifactory')
+            def uploadSpec = """{ 
+            "files": [{ 
+            "pattern": "${Env.PATTERN_ARTIFACTORY_FOLDER}", 
+            "target": "${Env.TARGET_ARTIFACTORY_FOLDER}" 
+            }] 
+            }""" 
+            server.upload(uploadSpec) 
+        } 
+        echo 'ARTIFACT UPLOADED TO THE ARTIFACTORY'
     }
+    body()
+}
