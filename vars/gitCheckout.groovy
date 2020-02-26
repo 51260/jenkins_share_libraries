c28a9f2fea23331dc1a88100ded7840c853111de
51260 <60729481+51260@users.noreply.github.com>
2020-02-26 09:23:38 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,7 +1,12 @@
-def call(Map stageParams) {  
- checkout([
-        $class: 'GitSCM',
-        branches: [[name:  '*/master' ]],
-        userRemoteConfigs: [[ url: stageParams.url ]]
-    ])
+import org.environment.Env;
+
+def call(Map config=[:], Closure body={}) {
+    stage("Code Checkout") {
+        checkout([$class: 'GitSCM', 
+                    branches: [[name: '*/master']], 
+                    userRemoteConfigs: [[url: Env.GIT_REPO]]])
+
+        echo 'GIT CHECK OUT DONE'
+    }
+    body()
 }
