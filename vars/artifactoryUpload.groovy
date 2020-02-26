97d655fc5c7a7b16d1397ecf688bd3669ea02de6
51260 <60729481+51260@users.noreply.github.com>
2020-02-26 09:10:11 +0530

    Update artifactoryUpload.groovy

--- vars/artifactoryUpload.groovy
+++ vars/artifactoryUpload.groovy
@@ -1,15 +1,13 @@
-def call (Map stageParams) {
-      rtUpload (
-          serverId: stageParams.Artifactory,
-          buildName: 'DevSecOps_CICD',
-          buildNumber: Env.BUILD_ID,
-          spec:'''{
-              "files": [
-                  {
-                  "pattern": "target/simple-spring-webapp-1.0.war",
-                  "target": "DevSecOpspetclinic"
-                  }
-              ]
-          }'''
-          )
+def call (Map stageParams) {      
+rtUpload (
+    serverId: 'Artifactory',
+    spec: '''{
+          "files": [
+            {
+              "pattern": "target/simple-spring-webapp-1.0.war",
+              "target": "DevSecOpspetclinic"
+            }
+         ]
+    }''',
+)
     }
