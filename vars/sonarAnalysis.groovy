445382f15c4771ff522175cfffc97994b92f92f4
51260 <60729481+51260@users.noreply.github.com>
2020-02-26 09:26:23 +0530

    Create sonarAnalysis.groovy

---
+++ vars/sonarAnalysis.groovy
@@ -0,0 +1,7 @@
+def call(Map config=[:], Closure body={}) {
+    stage("SonarQube Code Analysis") {            
+        sh "mvn sonar:sonar         
+        echo "SONARQUBE ANALYSIS DONE"
+    }
+    body()
+}
