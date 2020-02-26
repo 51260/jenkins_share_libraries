cf20e99ff491ff306f09eaf729951b62092f2b9a
51260 <60729481+51260@users.noreply.github.com>
2020-02-26 09:39:27 +0530

    Update buildApplication.groovy

--- vars/buildApplication.groovy
+++ vars/buildApplication.groovy
@@ -1,16 +1,6 @@
 def call(Map config=[:], Closure body={}) {
-    stage("Maven Build") {
-        switch(Env.buildTool){
-            case 'maven':
-                sh 'mvn clean install'
-                break
-            case 'gradle':
-                 sh 'gradle build'
-                break
-            case 'npm':
-                  sh 'npm install'
-                break
-                }
-                }
-    body()
+    stage("Maven Build") {          
+           sh 'mvn clean install'
+    }
+       body()
 }
