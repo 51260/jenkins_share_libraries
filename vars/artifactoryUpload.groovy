6d1e2eabc3dc2caef24f79de0cbddd444bfc5f91
51260 <60729481+51260@users.noreply.github.com>
2020-02-26 11:16:00 +0530

    Update artifactoryUpload.groovy

--- vars/artifactoryUpload.groovy
+++ vars/artifactoryUpload.groovy
@@ -1,7 +1,7 @@
 def call(Map config=[:], Closure body={}) {
     stage("Artifact Upload") {
         script { 
-            def server = Artifactory.newServer url: 'http://ec2-34-223-129-121.us-west-2.compute.amazonaws.com:8081/', username: 'admin', password: 'DevSecOps'
+            def server = Artifactory.newServer url: 'http://ec2-34-223-129-121.us-west-2.compute.amazonaws.com:8081/artifactory', username: 'admin', password: 'DevSecOps'
             def uploadSpec = """{ 
             "files": [{ 
             "pattern": "${env.PATTERN_ARTIFACTORY_FOLDER}", 
