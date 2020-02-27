61b31596caf758b82f7500960a0c28cd3bc04d18
51260 <60729481+51260@users.noreply.github.com>
2020-02-27 10:52:58 +0530

    Create DAST.groovy

---
+++ vars/DAST.groovy
@@ -0,0 +1,17 @@
+def call(Closure body={}) {
+        stage('DAST scan') {
+            steps {
+                script {
+                   sh 'zap-cli quick-scan --spider http://ec2-34-214-172-180.us-west-2.compute.amazonaws.com:8080/simple-spring-webapp-1.0/'
+                }
+            }
+    post {
+        always {
+            script {
+                archiveZap(failAllAlerts: 0, failHighAlerts: 0, failMediumAlerts: 0, failLowAlerts: 0)
+            }
+        }
+    }
+}
+    body()
+}
