459b370c62d05635ed2b3c2587c594651bae035b
51260 <60729481+51260@users.noreply.github.com>
2020-02-27 09:44:03 +0530

    Update ZAP.groovy

--- vars/ZAP.groovy
+++ vars/ZAP.groovy
@@ -1,12 +1,10 @@
 def start() {
     stage("Starting Zap") {
-            script {
                 startZap(
                     host: "127.0.0.1", 
                     port: '8090', 
                     zapHome: "/opt/ZAP_2.9.0",
                     allowedHosts:['http://ec2-34-211-215-31.us-west-2.compute.amazonaws.com:8080/simple-spring-webapp-1.0/'])
-            }
             sh "ps auxwww|grep -i zap"
     }
 }
