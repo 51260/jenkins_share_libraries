ee97497d5562b79ccecf3d1f4b8b174050841d5d
51260 <60729481+51260@users.noreply.github.com>
2020-02-27 11:15:10 +0530

    Update gitCheckout.groovy

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,5 +1,6 @@
 def call(Map config=[:], Closure body={}) {
     stage("Code Checkout") {
+        sh 'rm -rf *'
         checkout([$class: 'GitSCM', 
                     branches: [[name: '*/master']], 
                     userRemoteConfigs: [[url: env.GIT_REPO]]])
