5d4930c4bad582e2468181c20634f5631dbbcf6f
51260 <60729481+51260@users.noreply.github.com>
2020-03-02 11:54:15 +0530

    Update deployment.groovy

--- vars/deployment.groovy
+++ vars/deployment.groovy
@@ -1,6 +1,6 @@
 def call(Map config=[:], Closure body={}) {
     stage("App Deployment") {          
-          sh 'ansible-playbook deployment.yml -i hosts --private-key /home/ec2-user/DevSecOpsWebServer.pem --extra-vars "warName= Env.warName"'
+          sh 'ansible-playbook deployment.yml -i hosts --private-key /home/ec2-user/DevSecOpsWebServer.pem --extra-vars "warName=simple-spring-webapp-1.0.war"'
     }
        body()
 }
